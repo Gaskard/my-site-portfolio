22ec11f2fb4322b211d2c6c5aec3915dae7f2239
Dmytro Shaposhnikov <dimacr137@gmail.com>
2026-03-21 17:47:53 +0100

    init project

---
+++ src/components/app/App.tsx
@@ -0,0 +1,11 @@
+import "normalize.css";
+import '../../styles/base.scss'
+
+function App() {
+
+  return (
+      <header/>
+  )
+}
+
+export default App
---
+++ src/components/header/header.tsx
@@ -0,0 +1,20 @@
+import './header.scss'
+
+const header = () => {
+    return (
+        <header className="header">
+            <nav className="header__navbar">
+                <ul className="header__navbar__list">
+                    <li className="header__navbar__list-item"></li>
+                    <li className="header__navbar__list-item"></li>
+                    <li className="header__navbar__list-item"></li>
+                    <li className="header__navbar__list-item"></li>
+                    <li className="header__navbar__list-item"></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default header;
+
